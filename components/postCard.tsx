f5053a166733e2a31e85aba9d657eb2dd9588895
Svitlana Zhovanik <LanaSvetCat@gmail.com>
2022-12-15 18:33:32 +0200

    added config

--- components/postCard.tsx
+++ components/postCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 const PostCard = ({ title, image, link }: Props) => {
     return (<li className="max-w-[296px] bg-[#FDFDFD]/30 border border-white rounded-[30px] backdrop-blur-sm p-5">
         <div className="flex gap-2 mb-5 items-center">
-        <Image src={image.url} alt={image.alt} className="w-[132px] h-[132px] rounded-[20px]"/>
+        <Image src={image.url} alt={image.alt} width={132} height={132} className="rounded-[20px]"/>
         <h3 className="max-w-[176px] truncate text-black font-medium text-[16px] leading-[1.5]">{title}</h3>
         </div>
         <a href={link} className="w-[200px] bg-rose-600 rounded-[30px] block text-center py-3 text-white font-medium text-[16px] leading-[1.5]">Дізнатися більше</a>
---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/photo/2022/12/12/**",
+      },
+    ],
+  },
+};
